1aa4db9cfb64f9c399fec100ff49e09feda8f5c6
JonsMota <jonsmota@gmail.com>
2023-11-12 18:51:30 -0300

    definindo o ncopias

--- baralho.py
+++ baralho.py
@@ -7,12 +7,13 @@ def gerar_baralho(n_copias=2, coringas=True, embaralhado=True):
     print(naipes)
     numeros = list('A23456789') + ['10'] + list('JQK')
 
-    for naipe in naipes:
-        for numero in numeros:
-            carta = numero + naipe
-            baralho.append(carta)
-    if coringas:    
-        baralho.extend(['JK1', 'JK2']) 
+    for _ in range(n_copias):
+        for naipe in naipes:
+            for numero in numeros:
+                carta = numero + naipe
+                baralho.append(carta)
+        if coringas:    
+            baralho.extend(['JK1', 'JK2']) 
     
     if embaralhado:
         random.shuffle(baralho)
