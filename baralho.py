c054c1bb6de86a77d2e172fb81f7c11f7633eda2
JonsMota <jonsmota@gmail.com>
2023-11-12 18:31:20 -0300

    gerar uma carta para cada naipe

--- baralho.py
+++ baralho.py
@@ -6,6 +6,10 @@ def gerar_baralho(n_copias=2, coringas=True, embaralhado=True):
     naipes = list('♠♥♦♣')
     print(naipes)
     numeros = list('A23456789') + ['10'] + list('JQK')
-    print(numeros)
-    
+
+    for naipe in naipes:
+        for numero in numeros:
+            carta = numero + naipe
+            print(carta)
+
 gerar_baralho()
