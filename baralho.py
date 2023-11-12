493c2790fd0f91f63740de8a88e15fca47879902
JonsMota <jonsmota@gmail.com>
2023-11-12 18:28:46 -0300

    gerado lista com naipes e cartas

--- baralho.py
+++ baralho.py
@@ -1,3 +1,11 @@
 import random
 
 def gerar_baralho(n_copias=2, coringas=True, embaralhado=True):
+    baralho = []
+
+    naipes = list('♠♥♦♣')
+    print(naipes)
+    numeros = list('A23456789') + ['10'] + list('JQK')
+    print(numeros)
+    
+gerar_baralho()
